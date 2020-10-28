3974aff57e7083eb720d3d7cd294b3c379b01121
Zachary Schoenfeld <zachary.schoenfeld@ibm.com>
2020-10-28 11:26:56 -0400

    Update to old pass

--- skeleton/Skeleton.cpp
+++ skeleton/Skeleton.cpp
@@ -11,23 +11,7 @@ namespace {
     SkeletonPass() : FunctionPass(ID) {}
 
     virtual bool runOnFunction(Function &F) {
-      errs() << "In a function called " << F.getName() << "!\n";
-
-      errs() << "Function body:\n";
-      //F.dump(); Out-of-Date: no more dump support in modern llvm unless you enable it at compile time.
-      F.print(llvm::errs());
-
-      for (auto &B : F) {
-        errs() << "Basic block:\n" << B;
-        errs() << "\n";
-        for (auto &I : B) {
-          errs() << "Instruction: \n" << I;
-          errs() << "\n";
-        }
-        errs() << "\n";
-      }
-      errs() << "\n";
-
+      errs() << "I saw a function called " << F.getName() << "!\n";
       return false;
     }
   };
